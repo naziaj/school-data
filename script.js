00cacac2beb59d22e2d93c5f9f8d63bf1593de4d
naziaj <49521887+naziaj@users.noreply.github.com>
2019-07-16 12:15:00 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,442 @@
+let school = {
+    name:"ABC school",
+    city:"karachi",
+    Principal: "Tasneem Shabbar",
+    Affiliation: "Aga Khan University",
+    Founded: 1982,
+    contact:031-1234567,
+    branches:[
+        girlsBranch:[
+            {
+            address:{
+                streat:" Annemarie Schimmel St, Block 3",
+                area:"PECHS",
+                city:"Karachi",
+                country:"Pakistan"
+            },
+            vicePrincipal:"Abida Naaz",
+            noOfFloor:4,
+            noOfClass:20,
+
+            schoolTiming:{
+                open:"8 o'clock",
+                offTime: "1 o'clock",
+            },
+            adminStaff:[
+                {
+                    admin:"abc",
+                    salary:12345,
+                },
+                {
+                    accountant:"abcd",
+                    salary:123456,
+                },
+                {
+                    feecollector: "afsg",
+                    salaryt:12345,
+                },
+                {
+                    helper:"sgffdjh",
+                    salary:12345,
+                },
+                {
+                    other:"12345",
+                    salary:123456,
+                }
+            ],
+            sweepingStaff:[
+                {
+                    firstFloorSweepers:[
+                        {
+                            name:'kugk',
+                            salary:2345,
+                        },
+                        {
+                            name:'hgfyututf',
+                            salary:2435,
+                        }
+                    ],
+                    secondFloorSweepers:[
+                        {
+                            name:'hgkj',
+                            salary:2345,
+                        },
+                        {
+                            name:'yjgh',
+                            salary:2435,
+                        }
+                    ],
+                    thirdFloorSweepers:[
+                        {
+                            name:"nbjhvjhv",
+                            salary:2345,
+                        },
+                        {
+                            name:'jhjguh',
+                            salary:2435,
+                        }     
+                    ],
+                    fourthFloorSweepers:[
+                        {
+                            name:'hfvhgfv',
+                            salary:2345,
+                        },
+                        {
+                            name:'hcyn',
+                            salary:2435,
+                        } 
+                    ],    
+            teachers:{
+                    mathsTeachers: [
+                        {
+                            name:"miss ayesha",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                            isShePaid: "true",
+                        },
+                        {
+                            name:"miss ruba",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }   
+                    ],
+                    englishTeachers:[
+                        {
+                            name:"miss sana",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"miss sara",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ],
+                    scienceTeachers:[
+                        {
+                            name:"miss talat",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"miss Rabia",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ],
+                    urduTeachers:[
+                        {
+                            name:"miss Abida",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"miss Shehla",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }   
+                    ] ,
+                    islamiatTeachers: [
+                        {
+                            name:"sir Abid",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"sir Ali Haider",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ],
+                    pstTeachers:[
+                        {
+                            name:"miss Nousheen",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"sir aqeel",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ] ,
+                    floorincharges:[
+                        {
+                            firstFloorIncharge:{
+                                name:"misss noor",
+                                duty:"look after 1stfloor",
+                                salary:12345,
+                            }
+                        },
+                        {
+                            secondFloorIncharge:{
+                                name:"misss Adah",
+                                duty:"look after 2ndfloor",
+                                salary:12345,
+                            }
+                        },
+                        {
+                            thirdFloorIncharge:{
+                                name:"misss Tuba",
+                                duty:"look after 3rdfloor",
+                                salary:12345,
+                            }
+                        },
+                        {
+                            forthFloorIncharge:{
+                                name:"misss Anum",
+                                duty:"look after 4thfloor",
+                                salary:12345,
+                            }
+                        }  
+                    ],
+            library:[
+                {
+                    preprimaryLibrary:[
+                        {
+                        librarian:{
+                            name:"miss Beena",
+                            class:" preprimary library",
+                            noOfPeriods: 5,
+                            salary:12345,
+                        },
+                        books:[
+                            {
+                                poemBooks:[
+                                    {
+                                        bookName:"i am a good boy",
+                                        price:300,
+                                        sNo:982,
+                                    },
+                                    {
+                                        bookName:"my body",
+                                        price:200,
+                                        sNo:9822,
+                                    }
+                                ],
+                                storyBooks:[
+                                    {
+                                        bookName:"kipper's family",
+                                        price:600,
+                                        sNo:1082,
+                                    },
+                                    {
+                                        bookName:"my grandpaa",
+                                        price:250,
+                                        sNo:8702,
+                                    }
+                                ],
+                                mathsBooks:[
+                                    {
+                                        bookName:"shapes",
+                                        price:400,
+                                        sNo:1033,
+                                    },
+                                    {
+                                        bookName:"lets count",
+                                        price:320,
+                                        sNo:0872,
+                                    }
+                                ]
+                                }
+                            ],
+                        noofChairs:40,
+                        noOfTables:6,
+                        noOfCushions:12,
+                        LED:{
+                            size:"40inches",
+                            cost:40000,
+                        },
+                        noOfSpeakers:2,
+                    }
+                    ],
+                    primaryLibrary:[
+                        {
+                        librarian:{
+                            name:"miss shumaila",
+                            class:"primary library",
+                            noOfPeriods: 6,
+                            salary:12345,
+                        },
+                        books:[
+                            {
+                                poemBooks:[
+                                    {
+                                        bookName:"humpty dumpty",
+                                        price:600,
+                                        sNo:2382,
+                                    },
+                                    {
+                                        bookName:"my friend",
+                                        price:200,
+                                        sNo:1822,
+                                    }
+                                ],
+                                storyBooks:[
+                                    {
+                                        bookName:"cenderella",
+                                        price:600,
+                                        sNo:1082,
+                                    },
+                                    {
+                                        bookName:"the scary joker",
+                                        price:250,
+                                        sNo:8702,
+                                    }
+                                ],
+                                mathsBooks:[
+                                    {
+                                        bookName:"angles",
+                                        price:400,
+                                        sNo:1033,
+                                    },
+                                    {
+                                        bookName:"multilplication",
+                                        price:460,
+                                        sNo:5572,
+                                    }
+                                ]
+                            }
+                        ],
+                            noofChairs:50,
+                            noOfTables:8,
+                            noOfshelfs:15,
+                            }
+                        ],
+                        secondaryLibrary:[
+                            {
+                                librarian:{
+                                    name:"miss Saira",
+                                    class:"secondary library",
+                                    noOfPeriods: 6,
+                                    salary:123454,
+                                },
+                                books:[
+                                    {
+                                        historyBooks:[
+                                            {
+                                                bookName:"about 1982",
+                                                price:800,
+                                                sNo:2382,
+                                            },
+                                            {
+                                                bookName:"mohnjo daro",
+                                                price:700,
+                                                sNo:46762,
+                                            }
+                                        ],
+                                        storyBooks:[
+                                            {
+                                                bookName:"the wolf",
+                                                price:600,
+                                                sNo:1652,
+                                            },
+                                            {
+                                                bookName:" the mistery house",
+                                                price:890,
+                                                sNo:4502,
+                                            }
+                                        ],
+                                        mathsBooks:[
+                                            {
+                                                bookName:"algebra",
+                                                price:800,
+                                                sNo:1033,
+                                            },
+                                            {
+                                                bookName:"graphs",
+                                                price:1000,
+                                                sNo:3568,
+                                            }
+                                        ]
+                                    }
+                                ],
+                                    noofChairs:50,
+                                    noOfTables:8,
+                                    noOfShelves:15,
+            }
+            ],
+            sports:[
+                {
+                    PETeachers:[
+                        {
+                            name:"sir Raza",
+                            salary:123456,
+                            noOfPeriods:5,
+                        },
+                        {
+                            name:"miss Urooj",
+                            salary:123456,
+                            noOfPeriods:5,
+                        }
+                    ],
+                    sportsItems:[
+                        {
+                            itemName:"football",
+                            pricePerFootball:800,
+                            quantity:10,
+                        },
+                        {
+                            itemName:"vollyball",
+                            pricePerVollyball:"Rs.770",
+                            quantity:10,
+                        },
+                        {
+                            itemName:"bat",
+                            pricePerBat:"Rs.1000",
+                            quantity:6,
+                        },
+                        {
+                            itemName:"tennis ball",
+                            priceOfPerTennisball:"Rs.200",
+                            quantity:8,
+                        },
+                        {
+                            itemName:
+                        }
+                    ]
+                }
+            ]
+    
+
+    
+
+
+
+            
+
+            
+        
+
+        
+
+
+
+        
+                }
+        ],
+        boysBranch:[
+
+
+
+        ]
+
+        
+
+
+    ]
+    
+   
+}
