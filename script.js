b7f86f21cba64de62e54d0a53037bc41e2e84e10
naziaj <49521887+naziaj@users.noreply.github.com>
2019-07-18 12:25:06 +0500

    WIP

--- script.js
+++ script.js
@@ -404,39 +404,466 @@ let school = {
                             quantity:8,
                         },
                         {
-                            itemName:
+                            itemName:"racket",
+                            priceOfPerTennisball:"Rs.600",
+                            quantity:8,
                         }
-                    ]
+                    ],
+
                 }
-            ]
-    
-
-    
-
-
-
-            
-
-            
-        
-
-        
-
-
-
-        
+            ],
                 }
         ],
-        boysBranch:[
+        boysBranch:[{
+            address:{
+                streat:" Pechs,164th St,Block 3",
+                area:"PECHS",
+                city:"Karachi",
+                country:"Pakistan"
+            },
+            contact:032-1234567,
+            vicePrincipal:"Abdull Riaz",
+            noOfFloors:3,
+            noOfClasses:20,
+            schoolTiming:{
+                open:"8 o'clock",
+                offTime: "1 o'clock",
+            },
+            adminStaff:[
+                {
+                    admin:"abc",
+                    salary:12345,
+                },
+                {
+                    accountant:"abcd",
+                    salary:123456,
+                },
+                {
+                    feecollector: "afsg",
+                    salaryt:12345,
+                },
+                {
+                    helper:"sgffdjh",
+                    salary:12345,
+                },
+                {
+                    other:"iuyt",
+                    salary:123456,
+                }
+            ],
+            sweepingStaff:[
+                {
+                    firstFloorSweepers:[
+                        {
+                            name:'Abida Anty',
+                            salary:2345,
+                        },
+                        {
+                            name:'hgfyututf',
+                            salary:2435,
+                        }
+                    ],
+                    secondFloorSweepers:[
+                        {
+                            name:'hgkj',
+                            salary:2345,
+                        },
+                        {
+                            name:'yjgh',
+                            salary:2435,
+                        }
+                    ],
+                    thirdFloorSweepers:[
+                        {
+                            name:"nbjhvjhv",
+                            salary:2345,
+                        },
+                        {
+                            name:'jhjguh',
+                            salary:2435,
+                        }     
+                    ],    
+            teachers:{
+                    mathsTeachers: [
+                        {
+                            name:"miss Haya",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"sir Abid",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }   
+                    ],
+                    englishTeachers:[
+                        {
+                            name:"sir Sajid",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"miss Anum",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ],
+                    scienceTeachers:[
+                        {
+                            name:"miss Parveen",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"sir Majid",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ],
+                    urduTeachers:[
+                        {
+                            name:"sir Alam",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"miss Sajida",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }   
+                    ] ,
+                    islamiatTeachers: [
+                        {
+                            name:"sir Abbas",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"sir Ali",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ],
+                    pstTeachers:[
+                        {
+                            name:"miss Zareen",
+                            salary:12345,
+                            class:"1 to 5",
+                            noOfPeriods: 5,
+                        },
+                        {
+                            name:"sir Sumaiya",
+                            salary:12345,
+                            class:"6 to 10",
+                            noOfPeriods: 5,
+                        }
+                    ] ,
+                    floorincharges:[
+                        {
+                            firstFloorIncharge:{
+                                name:"miss Shazia",
+                                duty:"look after 1stfloor",
+                                salary:12345,
+                            }
+                        },
+                        {
+                            secondFloorIncharge:{
+                                name:"misss Alia",
+                                duty:"look after 2ndfloor",
+                                salary:12345,
+                            }
+                        },
+                        {
+                            thirdFloorIncharge:{
+                                name:"sir Hassan",
+                                duty:"look after 3rdfloor",
+                                salary:12345,
+                            }
+                        },  
+                    ],
+            library:[
+                {
+                    preprimaryLibrary:[
+                        {
+                        librarian:{
+                            name:"sir Zain",
+                            class:" preprimary library",
+                            noOfPeriods: 5,
+                            salary:12345,
+                        },
+                        books:[
+                            {
+                                poemBooks:[
+                                    {
+                                        bookName:"i am happy",
+                                        price:300,
+                                        sNo:982,
+                                    },
+                                    {
+                                        bookName:"my self",
+                                        price:200,
+                                        sNo:9822,
+                                    }
+                                ],
+                                storyBooks:[
+                                    {
+                                        bookName:"kipper's family",
+                                        price:600,
+                                        sNo:1082,
+                                    },
+                                    {
+                                        bookName:"my cute puppy",
+                                        price:250,
+                                        sNo:8702,
+                                    }
+                                ],
+                                mathsBooks:[
+                                    {
+                                        bookName:"1 to 100",
+                                        price:400,
+                                        sNo:1033,
+                                    },
+                                    {
+                                        bookName:"counting",
+                                        price:320,
+                                        sNo:0872,
+                                    }
+                                ]
+                                }
+                            ],
+                        noofChairs:30,
+                        noOfTables:6,
+                        noOfCushions:12,
+                        LED:{
+                            size:"40inches",
+                            cost:40000,
+                        },
+                        noOfSpeakers:2,
+                    }
+                    ],
+                    primaryLibrary:[
+                        {
+                        librarian:{
+                            name:"sir Niaz",
+                            class:"primary library",
+                            noOfPeriods: 6,
+                            salary:12345,
+                        },
+                        books:[
+                            {
+                                poemBooks:[
+                                    {
+                                        bookName:"daffodils",
+                                        price:600,
+                                        sNo:2382,
+                                    },
+                                    {
+                                        bookName:"my sweet home",
+                                        price:200,
+                                        sNo:1822,
+                                    }
+                                ],
+                                storyBooks:[
+                                    {
+                                        bookName:"king",
+                                        price:600,
+                                        sNo:1082,
+                                    },
+                                    {
+                                        bookName:"the witch house",
+                                        price:250,
+                                        sNo:8702,
+                                    }
+                                ],
+                                mathsBooks:[
+                                    {
+                                        bookName:"parameter",
+                                        price:400,
+                                        sNo:1033,
+                                    },
+                                    {
+                                        bookName:"solve my problems",
+                                        price:460,
+                                        sNo:5572,
+                                    }
+                                ]
+                            }
+                        ],
+                            noofChairs:50,
+                            noOfTables:8,
+                            noOfshelfs:15,
+                            }
+                        ],
+                        secondaryLibrary:[
+                            {
+                                librarian:{
+                                    name:"sir Amjad",
+                                    class:"secondary library",
+                                    noOfPeriods: 6,
+                                    salary:123454,
+                                },
+                                books:[
+                                    {
+                                        historyBooks:[
+                                            {
+                                                bookName:"mughals",
+                                                price:800,
+                                                sNo:2382,
+                                            },
+                                            {
+                                                bookName:"precious stone (1830)",
+                                                price:800,
+                                                sNo:46762,
+                                            }
+                                        ],
+                                        storyBooks:[
+                                            {
+                                                bookName:"dark",
+                                                price:600,
+                                                sNo:1652,
+                                            },
+                                            {
+                                                bookName:" two brothers",
+                                                price:890,
+                                                sNo:4502,
+                                            }
+                                        ],
+                                        mathsBooks:[
+                                            {
+                                                bookName:"algebra",
+                                                price:800,
+                                                sNo:1033,
+                                            },
+                                            {
+                                                bookName:"logarithum",
+                                                price:1000,
+                                                sNo:3568,
+                                            }
+                                        ]
+                                    }
+                                ],
+                                    noofChairs:40,
+                                    noOfTables:8,
+                                    noOfShelves:15,
+            }
+            ],
+            sports:[
+                {
+                    PETeachers:[
+                        {
+                            name:"sir Adil",
+                            salary:123456,
+                            noOfPeriods:5,
+                        },
+                        {
+                            name:"Sir Baqir",
+                            salary:123456,
+                            noOfPeriods:5,
+                        }
+                    ],
+                    sportsItems:[
+                        {
+                            itemName:"football",
+                            pricePerFootball:800,
+                            quantity:10,
+                        },
+                        {
+                            itemName:"vollyball",
+                            pricePerVollyball:"Rs.770",
+                            quantity:10,
+                        },
+                        {
+                            itemName:"bat",
+                            pricePerBat:"Rs.1000",
+                            quantity:6,
+                        },
+                        {
+                            itemName:"tennis ball",
+                            priceOfPerTennisball:"Rs.200",
+                            quantity:8,
+                        },
+                        {
+                            itemName:"racket",
+                            priceOfPerTennisball:"Rs.600",
+                            quantity:8,
+                        }
+                    ],
+                }
+        ],
+        utilities:[
+            {
+                electricityBillPerMonth:{
+                    girlsBranch:59000,
+                    boysBranch:50000, 
+                }
+            },
+            {
+                gasBillPerMonth:{
+                    girlsBranch:100000,
+                    boysBranch:80000, 
+                }  
+            },
+            {
+                waterBillPerMonth:{
+                    girlsBranch:20000,
+                    boysBranch:15000, 
+                }
+            },
+            {
+                wifiBillPerMonth:{
+                    girlsBranch:10000,
+                    boysBranch:10000, 
+                }
+            },
+            {
+                telephoneBillPerMonth:{
+                    girlsBranch:6000,
+                    boysBranch:6000, 
+                }
+            } 
+        ],
+        breakExpenseForStaff:{
+            milk:12345,
+            sugar:12345,
+            tea: 12645,
+        },
+        assets:[
+            {
+            photocopyMachine:{
+                    quantity:6,
+                    price:12345,
+            }
+        },
+        {
+            chairs:[
+                {
 
+                blueChairs:{
+                      
+                }
+                },
+                redChairs
+            ]
 
+            
+        }
 
-        ]
-
+        
+    
         
 
 
-    ]
+    
     
    
 }
